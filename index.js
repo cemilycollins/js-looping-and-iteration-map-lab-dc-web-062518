30c5fa8795e86edb840204859ab163c0e63c9b00
github username <github email address>
2018-08-06 11:38:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file.
+function lowerCaseDrivers(collection) {
+  newCollection = collection.map(function(driver) { return driver.toLowerCase()})
+  return newCollection
+}
+
+function nameToAttributes(collection) {
+
+  newCollection = collection.map(function(driver) {
+    let arr = driver.split(" ")
+    return Object.assign({}, {firstName: arr[0]}, {lastName: arr[1]})})
+  return newCollection
+}
+
+function attributesToPhrase(collection) {
+  newCollection = collection.map(function(driver) { return `${driver.name} is from ${driver.hometown}`})
+  return newCollection
+}
